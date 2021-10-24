eaa7bdaf1aa36619f7bf6a8bb9b28f5693844674
Lucas Servo <l.servo@hotmail.com>
2021-10-24 09:57:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const userNames = {
+  github: 'maykbrito',
+  twitter: 'lservo',
+  youtube: 'lservo',
+  facebook: 'lservo',
+  instagram: 'lservo'
+}
+
+function changeSocialMedia() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${userNames[social]}`
+  }
+}
+
+changeSocialMedia()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${userNames.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGithubProfileInfos()
